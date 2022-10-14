7894a720143c8ce1649c330e970cf165e2b091f7
ulatov <53035688+ulatov@users.noreply.github.com>
2022-10-14 18:56:05 +0500

    Update effect.js

--- effect.js
+++ effect.js
@@ -1,28 +1,7 @@
 "use strict";
 
 (()=>{
-    const m = document.querySelectorAll('div.messages')
-    for (let elem of document.querySelectorAll('div.interactelement')) {
-        /*
-        elem.addEventListener('mouseenter',function(e){
-            elem.classList.add('thebigone')
-        })
-        elem.addEventListener('mouseleave',function(e){
-            elem.classList.remove('thebigone')
-        })
-        elem.addEventListener('touchstart',function(e){
-            elem.classList.add('thebigone')
-        })
-        elem.addEventListener('touchend',function(e){
-            elem.classList.remove('thebigone')
-        })
-        */
-        /*
-        elem.addEventListener('touchmove',function(e){
-            console.log(e.target)
-        })
-        */
-    }
+    
     let currentElement = undefined
 
     document.body.addEventListener('touchmove',function(e){
@@ -30,7 +9,6 @@
         var realTarget = document.elementFromPoint(myLocation.clientX, myLocation.clientY)
 
         if (realTarget == undefined || !realTarget.classList.contains("interactelement")) {
-            console.log('no target')
             if (currentElement !== undefined) {
                 currentElement.classList.remove('thebigone')
             }
@@ -39,9 +17,7 @@
         }
         
         if (currentElement == realTarget) {
-            console.log('the same target')
         } else {
-            console.log('to other target')
             if (currentElement !== undefined) {
                 currentElement.classList.remove('thebigone')
             }
@@ -69,19 +45,5 @@
         currentElement = realTarget
         currentElement.classList.add('thebigone')
     })
-    /*
-    document.body.addEventListener('mousemove',function(e){
-        console.log(e.currentTarget)
-    })
-    document.body.addEventListener('touchmove',function(e){
-        console.log(e)
-    })
-    console.log('hello')
-    document.body.addEventListener('mouseenter',function(e){
-        console.log('mouseenter')
-    })
-    document.body.addEventListener('mouseleave',function(e){
-        console.log('mouseleave')
-    })
-    */
+    
 })()
